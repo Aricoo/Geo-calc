7b912d02c6ef73eda9470b497ca6b2f668daa988
Alejandro Rico <155132225+Aricoo@users.noreply.github.com>
2023-12-29 15:09:50 -0600

    Update Geocalc.py

--- Geocalc.py
+++ Geocalc.py
@@ -1,52 +1,52 @@
 print("What operation?\n")
-print("Volume\n")
-print("Area")
-choice = input("\nEnter the name of the operation?\n " )
+print("Volume")
+print("\n""Area")
+choice = input("\nEnter the name of the operation? " )
 
 print("Select a shape\n")
-print("Triangular prism\n")
-print("Cube\n")
-print("Sphere\n")
-print("Rectangle\n")
+print("Triangular prism""\n")
+print("Cube""\n")
+print("Sphere""\n")
+print("Rectangle""\n")
 choice = input("\nEnter the name of the shape: " )
 
-if choice == "Volume" "Triangular prism" :
-    Length = input("What is the length of the shape? " )
+if choice == "Triangular prism" and "Volume" :
+    Length = input("\n""What is the length of the shape? " )
     
-    Width = input("what's the width of the shape? " )
+    Width = input("\n""what's the width of the shape?: " )
     
-    Height = input("What's the height of the shape? " )
+    Height = input("\n""What's the height of the shape?: " )
     
-    Triangular_Prism_Answer = (int(Length) * int(Width) * int(Height) * 1/2)
+    Triangular_Prism_Answer_Volume = (int(Length) * int(Width) * int(Height) * 1/2)
 
-if choice == "Volume" "Triangular prism" :
-    print("\nYour answer is" ,Triangular_Prism_Answer)
+if choice == "Triangular prism" and "Volume" :
+    print("\nYour answer is" ,Triangular_Prism_Answer_Volume)
 
-if choice == "Volume" "Cube" :
-    Side = input("What's the measuremant of a side? " )
+if choice == "Cube" and "Volume" :
+    Side = input("\n""What's the measuremant of a side?: " )
     Cube_Answer = (int(Side)**3)
     
 
-if choice== "Volume" "Cube" :
+if choice== "Cube" and "Volume" :
     print("\nyour answer is" , Cube_Answer)
 
-if choice== "Volume" "Sphere" :
-    Radius = input("What's the radius of the shape? " )
+if choice== "Sphere" and "Volume" :
+    Radius = input("\n""What's the radius of the shape?: " )
     Sphere_Answer = (int(Radius)**3) * 4/3 * 3.14159
 
-if choice== "Volume" "Sphere" :
+if choice== "Sphere" and "Volume" :
     print("\nyour answer is" , Sphere_Answer)
 
-if choice== "Volume" "Rectangle" :
-    Length = input("What is the length of the shape? " )
+if choice== "Rectangle" and "Volume" :
+    Length = input("\n""What is the length of the shape? : " )
     
-    Width = input("what's the width of the shape? " )
+    Width = input("\n""what's the width of the shape?: " )
     
-    Height = input("What's the height of the shape? " )
+    Height = input("\n""What's the height of the shape?: " )
     
     Rectangle_Answer = (int(Length) *int(Width) * int(Height))
 
-if choice== "Volume" "Rectangle" :
+if choice== "Rectangle" and "Volume" :
     print("\nyour answer is" , Rectangle_Answer)
  
 
